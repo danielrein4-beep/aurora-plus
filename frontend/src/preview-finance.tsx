c024056756f2806bfbfbe87efdc251023f9a2fa7
danielrein4-beep <danielrein4@gmail.com>
2026-09-12 23:20:05 -0400

    chore(finance-ui): add isolated preview entrypoints for design review

---
+++ frontend/src/preview-finance.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CentroFinanciero from './pages/CentroFinanciero';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CentroFinanciero />
+  </React.StrictMode>
+);
